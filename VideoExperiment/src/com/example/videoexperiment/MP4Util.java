02ffce74c957b4fca88dd92581c66af2c6016774
fschwiet <fschwiet@gmail.com>
2014-09-08 17:24:33 -0700

    adding mp4parser

---
+++ VideoExperiment/src/com/example/videoexperiment/MP4Util.java
@@ -0,0 +1,53 @@
+package com.example.videoexperiment;
+
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+public class MP4Util {
+	public void AppendMP4Files(String outputFile, String[] inputFiles) {
+
+		List<Movie> inputMovies = new List<Movie>();
+		
+		for (String file : inputFiles) {
+			inputMovies.add(MovieCreator.build(file));
+		}
+		
+        List<Track> videoTracks = new LinkedList<Track>();
+        List<Track> audioTracks = new LinkedList<Track>();
+
+        for (Movie m : inputMovies) {
+            for (Track t : m.getTracks()) {
+                if (t.getHandler().equals("soun")) {
+                    audioTracks.add(t);
+                }
+                if (t.getHandler().equals("vide")) {
+                    videoTracks.add(t);
+                }
+            }
+        }
+
+        Movie result = new Movie();
+
+        if (audioTracks.size() > 0) {
+            result.addTrack(new AppendTrack(audioTracks.toArray(new Track[audioTracks.size()])));
+        }
+        if (videoTracks.size() > 0) {
+            result.addTrack(new AppendTrack(videoTracks.toArray(new Track[videoTracks.size()])));
+        }
+
+        Container out = new DefaultMp4Builder().build(result);
+
+        FileChannel fc = new RandomAccessFile(String.format("output.mp4"), "rw").getChannel();
+        out.writeContainer(fc);
+        fc.close();
+	}
+}
