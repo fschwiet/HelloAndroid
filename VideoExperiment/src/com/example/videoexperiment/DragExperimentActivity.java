ea94206bd5525939d251e61d1f9b198c0ff7731e
fschwiet <fschwiet@gmail.com>
2014-09-19 16:14:36 -0700

    can drag horizontally now

--- VideoExperiment/src/com/example/videoexperiment/DragExperimentActivity.java
+++ VideoExperiment/src/com/example/videoexperiment/DragExperimentActivity.java
@@ -3,13 +3,61 @@ package com.example.videoexperiment;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 
 public class DragExperimentActivity extends Activity {
+	
+	public class DragExperimentTouchListener implements View.OnTouchListener {
+
+		public DragExperimentTouchListener(float initalX, float initialY) {
+			lastX = initalX;
+			lastY = initialY;
+		}
+		
+		boolean isDragging = false;
+		float lastX;
+		float lastY;
+		float deltaX;
+		
+		@Override
+		public boolean onTouch(View arg0, MotionEvent arg1) {
+			
+			int action = arg1.getAction();
+			
+			if (action == MotionEvent.ACTION_DOWN && !isDragging) {
+				isDragging = true; 
+				deltaX = arg1.getX();
+				return true;
+			} else if (isDragging) {
+				if (action == MotionEvent.ACTION_MOVE) {
+					
+					arg0.setX(arg0.getX() + arg1.getX() - deltaX);
+					arg0.setY(arg0.getY());
+					return true;
+				} else if (action == MotionEvent.ACTION_UP) {
+					isDragging = false;
+					lastX = arg1.getX();
+					lastY = arg1.getY();
+					return true;
+				} else if (action == MotionEvent.ACTION_CANCEL) {
+					arg0.setX(lastX);
+					arg0.setY(lastY);
+					isDragging = false;
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +66,9 @@ public class DragExperimentActivity extends Activity {
 		
 		final View dragButton = (View)findViewById(R.id.experiment_draggable);
 		
+		dragButton.setOnTouchListener(new DragExperimentTouchListener(dragButton.getX(), dragButton.getY()));
+		
+		/*
 		dragButton.setLongClickable(true);
 		dragButton.setOnLongClickListener(new View.OnLongClickListener(){
 
@@ -35,6 +86,8 @@ public class DragExperimentActivity extends Activity {
     
 				return true;
 			}
-		});
+		})
+		*/;
 	}
+
 }
