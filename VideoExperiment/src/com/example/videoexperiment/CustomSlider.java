bceeadb62b04748e65ac0246eb5d2c5c2272d061
fschwiet <fschwiet@gmail.com>
2014-09-21 14:40:27 -0700

    custom slider now supports a range

--- VideoExperiment/src/com/example/videoexperiment/CustomSlider.java
+++ VideoExperiment/src/com/example/videoexperiment/CustomSlider.java
@@ -11,7 +11,10 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class CustomSlider extends RelativeLayout {
-
+	
+	View draggableStart;
+	View draggableEnd;
+	
 	public CustomSlider(Context context) {
         super(context);
         init();
@@ -25,21 +28,66 @@ public class CustomSlider extends RelativeLayout {
 	public CustomSlider(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init();
+	}	
+	
+	public float getStart() {
+		return draggableStart.getX();
 	}
+	
+	public float getEnd() {
+		return draggableEnd.getX();
+	}
+	
+	public float getMax() {
+		return this.getWidth() - draggableEnd.getWidth();
+	}
+	
 	private void init() {
 		inflate(getContext(), R.layout.control_custom_slider, this);
 		
-		View draggable = (View)findViewById(R.id.customSlider_draggable);
-		draggable.setOnTouchListener(new DragExperimentTouchListener(draggable.getX(), draggable.getY()));
+		draggableStart = (View)findViewById(R.id.customSlider_draggable_start);
+		draggableStart.setOnTouchListener(new DragExperimentTouchListener(draggableStart.getX(), draggableStart.getY(), new DynamicRange() {
+
+			@Override
+			public float getStart() {
+				return 0;
+			}
+
+			@Override
+			public float getEnd() {
+				return draggableEnd.getX();
+			}
+		}));
+		
+		draggableEnd = (View)findViewById(R.id.customSlider_draggable_end);
+		draggableEnd.setOnTouchListener(new DragExperimentTouchListener(draggableEnd.getX(), draggableEnd.getY(), new DynamicRange() {
+
+			@Override
+			public float getStart() {
+				return draggableStart.getX();
+			}
+
+			@Override
+			public float getEnd() {
+				return CustomSlider.this.getMax();
+			}
+		}));
+	}
+	
+	public interface DynamicRange {
+		public float getStart();
+		public float getEnd();
 	}
 	
 	public class DragExperimentTouchListener implements View.OnTouchListener {
 
-		public DragExperimentTouchListener(float initalX, float initialY) {
+		public DragExperimentTouchListener(float initalX, float initialY, DynamicRange range) {
+			this.range = range;
 			lastX = initalX;
 			lastY = initialY;
 		}
 		
+		DynamicRange range;
 		boolean isDragging = false;
 		float lastX;
 		float lastY;
@@ -57,7 +105,15 @@ public class CustomSlider extends RelativeLayout {
 			} else if (isDragging) {
 				if (action == MotionEvent.ACTION_MOVE) {
 					
-					arg0.setX(arg0.getX() + arg1.getX() - deltaX);
+					float resultingX = arg0.getX() + arg1.getX() - deltaX;
+					
+					if (resultingX < range.getStart())
+						resultingX = range.getStart();
+					
+					if (resultingX > range.getEnd())
+						resultingX = range.getEnd();
+					
+					arg0.setX(resultingX);
 					arg0.setY(arg0.getY());
 					return true;
 				} else if (action == MotionEvent.ACTION_UP) {
