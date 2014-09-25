9e5f003c7c6212f35900f650f9cc87962811093a
fschwiet <fschwiet@gmail.com>
2014-09-25 16:10:24 -0700

    moving CameraPreview element into layout file

--- VideoExperiment/src/com/example/videoexperiment/RecordJamActivity.java
+++ VideoExperiment/src/com/example/videoexperiment/RecordJamActivity.java
@@ -44,10 +44,8 @@ public class RecordJamActivity extends Activity {
 		
 		ViewGroup mLayout = (ViewGroup)findViewById(R.id.layout_record_jam);
 		
-		recordingView = new CameraPreview(this, CameraChooser.GetFrontFacingCameraGingerbread());
-		
-		mLayout.addView(recordingView, 0);
-		
+		recordingView = (CameraPreview)findViewById(R.id.cameraPreview);
+		recordingView.SetCameraId(CameraChooser.GetFrontFacingCameraGingerbread());
 		
 		reviewButton = (Button)this.findViewById(R.id.button_review_jam);
 		
